cbc8d6b884a1d6c33dea5d3300ab3bb3adbc52fc
Ethan Chen <acuratnam@gmail.com>
2024-03-06 20:17:10 +0800

    Dragon Mint AnAn v0.0.30

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -202,12 +202,12 @@ const NavBar = () => {
               />
             </a>
           </div>
-          <div className={Style.navbar_container_left_box_input}>
+          {/* <div className={Style.navbar_container_left_box_input}>
             <div className={Style.navbar_container_left_box_input_box}>
               <input type="text" placeholder="Search NFT" />
               <BsSearch onClick={() => {}} className={Style.search_icon} />
             </div>
-          </div>
+          </div> */}
         </div>
         {/* end of left section */}
         <div className={Style.navbar_container_right}>
